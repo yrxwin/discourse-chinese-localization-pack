59d183890768ba815aa4b4c36f4063b10a4ac333
Erick Guan <fantasticfears@gmail.com>
2016-03-16 18:36:37 +0100

    cleansing

--- lib/auth_providers/oauth_gems.rb
+++ lib/auth_providers/oauth_gems.rb
@@ -108,52 +108,6 @@ class OmniAuth::Strategies::QQConnect < OmniAuth::Strategies::OAuth2
   end
 end
 
-OmniAuth.config.add_camelization('qq_connect', 'QQConnect')
-
-# Discourse plugin
-class QQAuthenticator < ::Auth::Authenticator
-
-  def name
-    'qq_connect'
-  end
-
-  def after_authenticate(auth_token)
-    result = Auth::Result.new
-
-    data = auth_token[:info]
-    raw_info = auth_token[:extra][:raw_info]
-    name = data['nickname']
-    username = data['name']
-    qq_uid = auth_token[:uid]
-
-    current_info = ::PluginStore.get('qq', "qq_uid_#{qq_uid}")
-
-    result.user =
-      if current_info
-        User.where(id: current_info[:user_id]).first
-      end
-
-    result.name = name
-    result.username = username
-    result.extra_data = { qq_uid: qq_uid, raw_info: raw_info }
-
-    result
-  end
-
-  def after_create_account(user, auth)
-    qq_uid = auth[:extra_data][:qq_uid]
-    ::PluginStore.set('qq', "qq_uid_#{qq_uid}", {user_id: user.id})
-  end
-
-  def register_middleware(omniauth)
-    omniauth.provider :qq_connect, :setup => lambda { |env|
-      strategy = env['omniauth.strategy']
-      strategy.options[:client_id] = SiteSetting.qq_connect_client_id
-      strategy.options[:client_secret] = SiteSetting.qq_connect_client_secret
-    }
-  end
-end
-
 class OmniAuth::Strategies::QQConnect < OmniAuth::Strategies::OAuth2
   option :name, "qq_connect"
 
