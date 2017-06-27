cdc66202d834ec6871511758eb73caa025cf5ee4
Fan Xu <fan.xu@hpe.com>
2017-06-27 00:42:39 -0400

    fix bilibili link

--- lib/onebox/bilibili.rb
+++ lib/onebox/bilibili.rb
@@ -18,7 +18,7 @@ module Onebox
       end
 
       def to_html
-        "<embed height='415' width='544' quality='high' allowfullscreen='true' type='application/x-shockwave-flash' src='https://static-s.bilibili.com/miniloader.swf' flashvars='aid=#{video_id}&page=1' pluginspage='http://www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash'></embed>"
+        "<embed height='415' width='544' quality='high' allowfullscreen='true' type='application/x-shockwave-flash' src='//static.hdslb.com/miniloader.swf' flashvars='aid=#{video_id}&page=1' pluginspage='//www.adobe.com/shockwave/download/download.cgi?P1_Prod_Version=ShockwaveFlash'></embed>"
       end
 
       def placeholder_html
